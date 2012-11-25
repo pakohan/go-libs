ef97313a3528ef052a108cc2558c52573ca55ea7
Patrick Kohan <patrick.kohan@gmail.com>
2012-11-25 13:53:19 +0100

    error type is private now

--- mysql/trans.go
+++ mysql/trans.go
@@ -1,3 +1,6 @@
+/*
+Provides simple interface to mysql transactions
+*/
 package mysql
 
 import (
@@ -7,46 +10,47 @@ import (
 	_ "github.com/ziutek/mymysql/native" // Native engine
 )
 
-type TransactionError struct {
+type transactionError struct {
 	message string
 }
 
+//Provides a simple container for the query parameters of an transaction
 type TransactionQuery struct {
 	Database, Table string
 	Columns         []string
 }
 
+//Our class to handle transactions with
 type Transaction struct {
 	stmt mysql.Stmt
 	transaction mysql.Transaction
 	numParams int
 }
 
-func (te TransactionError) Error() string {
+//To implement the error interface
+func (te transactionError) Error() string {
 	return te.message
 }
 
-func NewTransaction(connection mysql.Conn, query TransactionQuery) (Transaction, error) {
-	errTrans := Transaction{}
-
+func NewTransaction(connection mysql.Conn, query TransactionQuery) (*Transaction, error) {
 	if len(query.Columns) < 1 {
-		return errTrans, TransactionError{"Too few columns"}
+		return nil, transactionError{"Too few columns"}
 	}
 
 	ins, err := connection.Prepare(fmt.Sprintf("INSERT INTO %s.%s (%s) VALUES (?%s)", query.Database, query.Table, strings.Join(query.Columns, ", "), strings.Repeat(", ?", len(query.Columns)-1)))
 	if err != nil {
-		return errTrans, TransactionError{"Error creating prepared Statement"}
+		return nil, transactionError{"Error creating prepared Statement"}
 	}
 
 	trans, err := connection.Begin()
 	if err != nil {
-		return errTrans, TransactionError{"Error creating Transaction"}
+		return nil, transactionError{"Error creating Transaction"}
 	}
 
-	return Transaction{trans.Do(ins), trans, len(query.Columns)}, nil
+	return &Transaction{trans.Do(ins), trans, len(query.Columns)}, nil
 }
 
-func (trans Transaction) BeginInsert(c chan []interface{}, mut chan int) {
+func (trans *Transaction) BeginInsert(c chan []interface{}, mut chan int) {
 	for {
 		params, ok := <-c
 		if !ok {
